7756b8b165bf6ab4ff9841c6dbe2d358c2d7a593
Nicole <nicoleguob@gmail.com>
2019-09-12 12:23:19 -0700

    added uptime bot command, removed spacy (not implemented yet), and edited the ping command to return bot latency

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import random
-import spacy
+import datetime
+# import spacy
 from discord.ext import commands
 from discord.ext.commands import Bot
 from discord.ext.commands import CommandNotFound
@@ -11,6 +12,7 @@ TOKEN = INFO[0]
 PREFIX = INFO[1]
 client = commands.Bot(command_prefix = PREFIX)
 nlp = None
+start_time = datetime.datetime.utcnow()
 
 
 @client.event
@@ -19,9 +21,11 @@ async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print('------')
+    '''
     nlp = spacy.load('en_core_web_lg')
     print('Modules Loaded:')
     if nlp: print('\tSpacy')
+    '''
     await client.change_presence(activity=discord.Game(name='!help for bot info', type=1))
     
 
@@ -32,9 +36,9 @@ async def on_command_error(ctx, error):
     raise error
 
 @client.command(name='ping',
-                description='pong. (returns latency)')
+                description='Pong! (returns latency)')
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send('Pong! {0}'.format(round(client.latency, 5)))
 
 @client.command(name = '8ball',
                 description = 'Answers a yes or no questions',
@@ -52,5 +56,27 @@ async def eight_ball(ctx):
     ]
     
     await ctx.send(random.choice(responses))
+    
+@client.command(name='uptime',
+                description='Returns how long the bot has been running for.')
+async def uptime(ctx):
+    '''
+    Source: https://stackoverflow.com/questions/52155265/my-uptime-function-isnt-able-to-go-beyond-24-hours-on-heroku
+    '''
+    now = datetime.datetime.utcnow() # Timestamp of when uptime function is run
+    delta = now - start_time
+    
+    hours, remainder = divmod(int(delta.total_seconds()), 3600)
+    minutes, seconds = divmod(remainder, 60)
+    days, hours = divmod(hours, 24)
+    
+    if days:
+        time_format = "**{d}** days, **{h}** hours, **{m}** minutes, and **{s}** seconds."
+        
+    else:
+        time_format = "**{h}** hours, **{m}** minutes, and **{s}** seconds."
+        
+    uptime_stamp = time_format.format(d=days, h=hours, m=minutes, s=seconds)
+    await ctx.send('{} has been up for {}'.format(client.user.name, uptime_stamp))
     
 client.run(TOKEN)
