9a6e3e17049fdba98235b0b3a4e0790e70ed9e3c
Nicole <nicoleguob@gmail.com>
2019-09-15 23:43:36 -0700

    bot will now auto assign the 'huscii coder' role when a new member joins. don't know if it works yet but i guess we'll see lol...

--- main.py
+++ main.py
@@ -40,6 +40,13 @@ async def on_command_error(ctx, error):
         return
     raise error
 
+@client.event
+async def on_member_join(member):
+    role = discord.utils.get(member.server.roles, name='HuSCII Coder')
+    await client.add_roles(member, role)
+    
+    
+    
 '''
 @client.event
 async def on_message(message):
